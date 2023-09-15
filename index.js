8dedb60913fb82fe3d153b6c9cc80d650847e2b3
Brian Kiprono <89153335+bkiprono@users.noreply.github.com>
2023-09-15 16:07:44 +0300

    feat: worked on string protos to replace template literals

---
+++ index.js
@@ -0,0 +1,23 @@
+String.prototype.interpolate = function (params) {
+  const names = Object.keys(params);
+  const vals = Object.values(params);
+  return new Function(...names, `return \`${this}\`;`)(...vals);
+}
+
+const template = '${data.text} ${data.name} ${data.file.fileName} ${data.userEmail}';
+const text = {
+  text: "Brian"
+}
+const payload = {
+  name: "Demo Data"
+};
+
+const file = {
+  fileName: "Demo File",
+  size: "123MB"
+}
+const userEmail = "patrick";
+const result = template.interpolate({
+  data: { ...text, ...payload, file, userEmail }
+});
+console.log(result);
